a094a535c0871ff0ffb6b52dac8023d733d2ef5f
Ben Yelsey <by@gc.io>
2012-08-18 18:39:04 -0400

    remove testing options from project.clj (oops)

--- project.clj
+++ project.clj
@@ -4,9 +4,4 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[com.yahoo.platform.yui/yuicompressor "2.4.2"]]
-  :aggregate-files [{:input ["foo/bar.css" "foo/baz.css"]
-                     :output "foo/compiled-files/main.css"}]
-  :aggregate-dirs [{:input ["foo"]
-                    :output "foo/compiled-dirs/main.css"
-                    :suffix "css"}]
   :eval-in-leiningen true)
