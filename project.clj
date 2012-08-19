5a34e3b6d0524617138b68fe1118c54dbf0180af
byels <byelsey1@gmail.com>
2012-08-19 01:53:50 -0400

    add clojure version dep

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[com.yahoo.platform.yui/yuicompressor "2.4.2"]]
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [com.yahoo.platform.yui/yuicompressor "2.4.2"]]
   :eval-in-leiningen true)
